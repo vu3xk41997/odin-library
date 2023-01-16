e971a841970ed3a22299bc07c9b2329593bcf11c
Mojito <vu3xk41997@gmail.com>
2023-01-16 22:32:50 +0100

    import json to js successfully and display book titles on html

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odin-library
 Javascript Project focusing on objects
+
+## run html on browser using python3
+to avoid error with importing JSON to js file
+$ cd ~/odin_repos/odin-library
+$ python3 -m http.server
+open http://localhost:8000/index.html in browser
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import books from './books.json';
+import books from './books.json' assert {type: "json"};
 let myLibrary = books;
+console.log(myLibrary);
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,6 +12,13 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    Book(title, author, pages, read);
-}
+// function addBookToLibrary(title, author, pages, read) {
+//     Book(title, author, pages, read);
+// }
+
+
+// display book details in HTML
+document.getElementById("item1").innerHTML = myLibrary[0].title;
+document.getElementById("item2").innerHTML = myLibrary[1].title;
+document.getElementById("item3").innerHTML = myLibrary[2].title;
+document.getElementById("item4").innerHTML = myLibrary[3].title;
